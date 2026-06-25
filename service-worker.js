d77706d8c9d64d33933e0061896f02a75dd5ed42
hara-den-ctrl <hara00densetu@gmail.com>
2026-06-25 21:11:09 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,1 @@
-const APP_VERSION="2.2";
-const CACHE_NAME=`ApartmentFeederCalculator-v${APP_VERSION}`;
-const ASSETS=["./","./index.html","./manifest.json","./icon-180.svg","./icon-192.svg","./icon-512.svg"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
+const APP_VERSION="2.3";const CACHE_NAME=`ApartmentFeederCalculator-v${APP_VERSION}`;const ASSETS=["./","./index.html","./manifest.json","./icon-180.svg","./icon-192.svg","./icon-512.svg"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
