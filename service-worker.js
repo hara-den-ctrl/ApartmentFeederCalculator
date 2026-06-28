ae91b1e4d09e1470ac1c65dd242f5dafcfb4c4e8
hara-den-ctrl <hara00densetu@gmail.com>
2026-06-28 12:33:01 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION="2.4";
+const APP_VERSION = "3.0";
 const CACHE_NAME=`ApartmentFeederCalculator-v${APP_VERSION}`;
 const ASSETS=["./","./index.html","./manifest.json","./icon-180.svg","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
